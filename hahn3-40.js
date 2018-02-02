d1e654912e85640659c9ef3a570e2cf389f9f302
Evan Genest <genest.1@osu.edu>
2018-02-01 20:02:41 -0600

    Very simple server.  Practice with routes.

---
+++ hahn3-40.js
@@ -0,0 +1,29 @@
+/* Hahn Ch3 p.40 */
+
+const express = require('express');
+const path = require('path');
+const http = require('http');
+
+const app = express();
+
+const publicPath = path.resolve(__dirname, "public");
+app.use(express.static(publicPath));
+
+app.get("/", (req, res)=>{
+	res.end("Welcome to the Dharma Bums.");
+} );
+
+app.get("/about", (req, res)=> {
+	res.end("'The DB's were a band.' -- Maximum RnR");
+}  );
+
+app.get("/store", (req, res)=>{
+	res.end("Consider exiting through our gift shop.");
+}  );
+
+// Let's grab some data from the route.
+app.get("/players/:name1", (req, res)=>{
+	res.end(`It's a fact that ${req.params.name1} is not a great player.`);
+}  )
+
+http.createServer(app).listen(8000);
