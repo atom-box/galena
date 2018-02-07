a7d8f957477f0b5a33033a3ea375c0fe870f8b54
Evan Genest <genest.1@osu.edu>
2018-02-07 06:14:56 -0600

    Microserver works.  Milestone victory for me.

--- oliviaRoute.js
+++ oliviaRoute.js
@@ -1,2 +1,15 @@
 /* A router with two routes.    */
 /* Very short!  Hahn p.71 Ch. 5 */
+
+const express = require('express');
+const app = express();
+
+app.get('/olivia/', (req, res)=>{
+	res.send("You have found Olivia Example.");
+} );
+
+app.use((req, res)=>{
+	res.status(400).send('Bad request.');
+} );
+
+app.listen(3002 );
