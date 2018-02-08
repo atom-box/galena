ded5ef4208c6b92fe17cb25f30de6c7fb5700421
Evan Genest <genest.1@osu.edu>
2018-02-07 20:44:40 -0600

    Make a pair of router files talk to each other without crashing but don't yet make the actual routes do anything.

--- oliviaRoute.js
+++ oliviaRoute.js
@@ -1,8 +1,12 @@
 /* A router with two routes.    */
-/* Very short!  Hahn p.71 Ch. 5 */
+/* 1) Very short!  Hahn p.71 Ch. 5 */
+/* 2) Now adapting it: lab to try tying router files together  */
 
 const express = require('express');
 const app = express();
+const petsRouter = require("./pets.js");
+app.use("/olivia/pets", petsRouter);
+
 
 app.get('/olivia/', (req, res)=>{
 	res.send("You have found Olivia Example.");
--- pets.js
+++ pets.js
@@ -1,6 +1,15 @@
-const petsRouter = require('express').Router({mergeParams: true});
-
-const app = petsRouter();
-app.get();
-
+const express = require("express");
+let pets = ['octopus', 'mushroom', 'cat1', 'cat2'];
+const petsRouter = express.Router();
+petsRouter.get("/olivia/pets",(req, res, next)=> {
+	console.log("pets rock!");
+	return;
+} )
 module.exports = petsRouter;
+
+//const petsRouter = require('express').Router({mergeParams: true});
+
+//const app = petsRouter();
+//app.get();
+
+//module.exports = petsRouter;
