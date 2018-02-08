47821fc1a1e17a984f6366d1b974456821eb853a
Evan Genest <genest.1@osu.edu>
2018-02-08 06:01:14 -0600

    Fix route-labels so that when user enters url LOCALHOST3210/OLIVIA/0 we see an octopus in the browser.

--- pets.js
+++ pets.js
@@ -1,11 +1,23 @@
 const express = require("express");
 let pets = ['octopus', 'mushroom', 'cat1', 'cat2'];
 const petsRouter = express.Router();
-petsRouter.get("/olivia/pets",(req, res, next)=> {
+
+console.log("Made it to here: #5 in pets.js.")
+petsRouter.get("/:id",(req, res)=>{
+	const x = pets[req.params.id];
+	res.send(`In your menagerie I found this [${x}].`);
+	next();
+});
+
+petsRouter.get("/",(req, res, next)=> {
 	console.log("pets rock!");
-	return;
-} )
+	res.send(`Fave pets have always been these: ${pets}`);
+	next();
+	// TRY WITHOUT NEXT
+});
+
 module.exports = petsRouter;
+console.log("Made it, even, to #20, after the 'Listen' statement.")
 
 //const petsRouter = require('express').Router({mergeParams: true});
 
