77c280c5a7ff49577f2d865c7bdf5734a2a51ca6
Evan Genest <genest.1@osu.edu>
2018-03-25 17:30:45 -0500

    Make one route that accepts and parses a route using Regex.Make a simple express server.

--- vote.js
+++ vote.js
@@ -1,3 +1,15 @@
 // March 25, 2018 
 // A one hour from-memory server
 
+const express = require("express");
+const PORT = 4321;
+const app = express();
+app.get( /^\/users\/(..)(..)$/, (req, res, next)=>{
+
+} );
+app.use((req, res)=>{
+
+} );
+
+app.listen(4321);
+console.log(`listeningggg on _${PORT}_.`)
